3b19252e893ebfaf4734fad78c92c93675e429c8
grachevrv <grachevrv@ya.ru>
2020-09-29 10:31:09 +0400

    feat: task comleted

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length) {
+        if (!array.length == 0) {
+            array.sort(function (a, b) {
+                return a - b;
+            });
+            return array[0]
+        }
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length) {
+        if (!array.length == 0) {
+            array.sort(function (a, b) {
+                return b - a;
+            });
+            return array[0]
+        }
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length) {
+        if (!array.length == 0) {
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            return array.reduce(reducer) / array.length
+        }
+    }
+    return 0;
+
 }
